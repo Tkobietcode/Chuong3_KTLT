ed81de8257bd34cf8a28de2e4def2f301ff45b94
PHAMVANTISEN <phamsen200904@gmail.com>
2024-08-06 09:33:23 +0700

    Chuong 3 chuoi

---
+++ Bai 2_Chuoi.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+
+// 2. Hàm đếm số khoảng trắng trong chuỗi.
+int demKhoangTrang(const string& str) {
+    int count = 0;
+    for (char c : str) {
+        if (isspace(c)) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    // Test cac ham da viet
+    string str;
+    cout << "Nhap chuoi: ";
+    getline(cin, str);
+
+    cout << "Chuoi ban vua nhap la: " << str << endl;
+
+    int soKhoangTrang = demKhoangTrang(str);
+    cout << "So khoang trang trong chuoi: " << soKhoangTrang << endl;
+
+    
+    return 0;
+}
