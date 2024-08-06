b1279a9bf60b5f50b296384f2bd556670499109c
PHAMVANTISEN <phamsen200904@gmail.com>
2024-08-06 09:44:31 +0700

    Chuong 3 chuoi

---
+++ Bai 6_Chuoi.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+
+// 6. Hàm cắt chuỗi họ tên thành chuỗi họ lót và chuỗi tên.
+pair<string, string> catChuoiHoTen(const string& hoTen) {
+    string hoLot = "", ten = "";
+    istringstream iss(hoTen);
+    string temp;
+    while (iss >> temp) {
+        if (temp != hoTen.substr(hoTen.find_last_of(" ") + 1)) {
+            hoLot += temp + " ";
+        }
+        else {
+            ten = temp;
+        }
+    }
+    return make_pair(hoLot, ten);
+}
+
+int main() {
+    
+
+        string hoTen, tenCanTim;
+       cout << "Nhap ho va ten: ";
+       getline(cin, hoTen);
+       
+
+     pair<string, string> hoVaTen = catChuoiHoTen(hoTen);
+     cout << "Ho va lot: " << hoVaTen.first << endl;
+     cout << "Ten: " << hoVaTen.second << endl;
+
+     
+
+    return 0;
+}
